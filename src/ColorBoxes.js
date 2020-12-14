bee87aabaf6e4e75147addda98da287638253890
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-12-14 16:34:53 +0530

    styled ColorBoxes roughly

--- src/ColorBoxes.js
+++ src/ColorBoxes.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import "./ColorBoxes.css";
 export default class ColorBoxes extends Component {
   render() {
+    const { color, name } = this.props;
     return (
-      <div style={{ background: this.props.color }} className="ColorBoxes">
-        <span>{this.props.name}</span>
-        <span>More</span>
+      //   <div style={{ background: this.props.color }} className="ColorBoxes">
+      //     <span>{this.props.name}</span>
+      //     <span>More</span>
+      //   </div>
+      <div style={{ background: color }} className="ColorBoxes">
+        <div className="ColorBoxes-copy">
+          <span>{name}</span>
+          <button>Copy</button>
+        </div>
+        <button className="see-more">More</button>
       </div>
     );
   }
