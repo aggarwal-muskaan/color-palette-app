9ee9e5f17f2b8ff07cbd2643e25616126e36ae3b
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-12-14 14:57:32 +0530

    made seedColors & Palette components

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* <header></header> */}
+
+        <h1>Hii Have to go long</h1>
+        <div className="Palette-colors">{/* bunch of color boxes */}</div>
+
+        {/* <footer></footer> */}
+      </div>
+    );
+  }
+}
