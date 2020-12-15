4b96ea5dcf873fe8e52bc3c19f4b3dceea891d76
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-12-15 13:57:49 +0530

    add colorsHelpers.js to generate the shades of color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import "./App.css";
 import seedColors from "./seedColors";
 import Palette from "./Palette";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedColors[3]));
   return (
     <div className="App">
       <Palette {...seedColors[5]} />
       {/* <Palette seedColors={seedColors[5]} /> */}
+      <Palette {...seedColors[2]} />
     </div>
   );
 }
