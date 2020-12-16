a5123e27bce323a6491a4aab1d92093310c84cd6
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-12-16 17:07:19 +0530

    created MiniPalette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 
 export default class PaletteList extends Component {
   render() {
     const { palettes } = this.props;
     return palettes.map((p) => (
       <p>
-        <Link to={`/palette/${p.id}`}>{p.paletteName}</Link>
+        <Link to={`/palette/${p.id}`}>
+          <MiniPalette {...p} />
+        </Link>
       </p>
     ));
   }
