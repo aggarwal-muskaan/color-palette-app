6a4903d0eb91f307b1c94c52a89ccccda3ca5c7a
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-12-18 16:08:31 +0530

    add GOBACK button in SingleColorShades

--- src/SingleColorShades.js
+++ src/SingleColorShades.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ColorBoxes from "./ColorBoxes";
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 export default class SingleColorShades extends Component {
   constructor(props) {
@@ -30,22 +31,29 @@ export default class SingleColorShades extends Component {
   }
 
   render() {
-    const { paletteName, emoji } = this.props.palette;
+    const { paletteName, emoji, id } = this.props.palette;
     let { format } = this.state;
     const shades = this._shades.map((color) => (
       <ColorBoxes
-        key={color.id}
+        key={color.name}
         name={color.name}
         color={color[format]}
         seemoreLink={false}
       />
     ));
     return (
-      <div className="Palette">
+      <div className="SingleColorShades Palette">
         {/* <h1>Single Color Palette</h1> */}
         <Navbar handleChange={this.colorFormatChange} showAllColors={false} />
 
-        <div className="Palette-colors">{shades}</div>
+        <div className="Palette-colors">
+          {shades}
+          <div className="go-back ColorBoxes">
+            <Link className="go-back-button" to={`/palette/${id}`}>
+              Go Back
+            </Link>
+          </div>
+        </div>
 
         {/* footer */}
         <footer className="Palette-footer">
