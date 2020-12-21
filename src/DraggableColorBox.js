38a9d439e65c1080d95b87d52f478aa5479f210d
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-12-21 17:42:20 +0530

    add icon & style to userColorBox

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { withStyles } from "@material-ui/styles";
 import style from "./styles/DraggableColorBoxStyle";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 // import classes from "*.module.css";
 
 function DraggableColorBox(props) {
   let { classes, clr } = props;
   return (
     <div className={classes.box} style={{ backgroundColor: clr.color }}>
-      {clr.name}
+      <div className={classes.boxContent}>
+        <span> {clr.name}</span>
+        <DeleteForeverIcon className={classes.deleteIcon} />
+      </div>
     </div>
   );
 }
