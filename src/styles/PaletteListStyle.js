5738f4ff5fb64f9e43a1d97bc5fb6d060a1f9795
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-12-30 18:08:14 +0530

    make PaletteList responsive

--- src/styles/PaletteListStyle.js
+++ src/styles/PaletteListStyle.js
@@ -1,3 +1,4 @@
+import breakpoint from "./breakpoints";
 const styles = {
   root: {
     backgroundColor: "blue",
@@ -14,7 +15,14 @@ const styles = {
     flexWrap: "wrap",
     marginBottom: "2rem",
     // height: "100%",
+    [breakpoint.down("xl")]: {
+      width: "80%",
+    },
+    [breakpoint.down("xs")]: {
+      width: "75%",
+    },
   },
+
   nav: {
     display: "flex",
     width: "100%",
@@ -28,7 +36,14 @@ const styles = {
     height: "80%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%",
+    gridGap: "2.5rem",
+    [breakpoint.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [breakpoint.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1rem",
+    },
     // overflowX: "hidden",
   },
 };
